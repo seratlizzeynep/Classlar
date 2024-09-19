3234e69f123013ab04baedab70e1ad458cee65f2
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-09-19 21:27:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CustomerManager yonetici = new CustomerManager();
+		yonetici.Add();
+		yonetici.Remove();
+		yonetici.Update();
+	}
+
+}
